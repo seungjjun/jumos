9e5ed63233035c0164136a00d3a56c3865572b47
seungjjun <il1uso@naver.com>
2023-05-29 01:02:31 +0900

    #1 config 프로젝트 세팅
    - gitignore 추가
    - 의존성 추가

---
+++ src/main/java/com/jumos/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package com.jumos.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
+	}
+
+}
